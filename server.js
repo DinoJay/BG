0d04eaec9af77171f789299bc1ed687b62a91422
djan <jott88@googlemail.com>
2014-11-02 15:00:14 +0100

    register form added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Import required modules.
 var express = require('express');
 var stormpath = require('express-stormpath');
+var path = require('path');
 
 // Initialize our Express app.
 var app = express();
+app.set('port', process.env.PORT || 3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'jade');
 
 // Configure Stormpath.
 app.use(stormpath.init(app, {
@@ -11,12 +17,17 @@ app.use(stormpath.init(app, {
   apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
   secretKey:    process.env.STORMPATH_SECRET_KEY,
   application:  process.env.STORMPATH_URL,
+  //views
+  registrationView: __dirname + '/pages/register.jade',
+  loginView: __dirname + '/pages/login.jade',
 }));
+
+app.use(require('less-middleware')(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Generate a simple home page.
 app.get('/', function(req, res) {
-  res.send("Hey NNILS there! Thanks for visting the site!"+
-           "Be sure to <a href='/login'>login</a>!");
+  res.render('index', {title: 'Home', user: req.user});
 });
 
 // Generate a simple dashboard page.
@@ -25,4 +36,6 @@ app.get('/dashboard', stormpath.loginRequired, function(req, res) {
 });
 
 // Listen for incoming requests and serve them.
-app.listen(process.env.PORT || 3000);
+app.listen(app.get('port'), function (err) {
+  console.log("Server started; listening on port " + app.get('port'));
+});
